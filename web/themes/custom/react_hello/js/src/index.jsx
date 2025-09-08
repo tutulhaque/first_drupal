ea699bf2f53310e51c8256c2b578646a7cb77aba
Md Saifuddullah <sdtutulctg@gmail.com>
2025-09-08 13:04:01 +0300

    drupal-update

---
+++ web/themes/custom/react_hello/js/src/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("react_app"));
+root.render(<h1>Hello world I am react_in_Drupal</h1>);
